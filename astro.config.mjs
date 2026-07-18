135e0a74826eaebdd679c7c99ef3faf464ab23c2
Minh Anh <MINHANH@TP.local>
2026-07-17 19:57:15 -0700

    ci: disable sitemap plugin to clear build crash

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,9 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
-import tailwind from "@astrojs/tailwind";
+import tailwind from '@astrojs/tailwind';
 
-// https://astro.build/config
 export default defineConfig({
-  site: 'https://mptran1802.github.io/astrofy',
-  integrations: [mdx(), sitemap(), tailwind()]
+  site: 'https://mptran1802.github.io', // Added your GitHub Pages URL just in case
+  base: '/astrofy', // Matches your repository name for GitHub Pages
+  integrations: [mdx(), tailwind()], // <-- Removed sitemap() here
 });
