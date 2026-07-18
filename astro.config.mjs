f151a02dad9d070c28313cf9f7e608ef9e3e61e8
Minh Anh <MINHANH@TP.local>
2026-07-17 19:12:47 -0700

    config: update site URL for deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://astrofy-template.netlify.app',
+  site: 'https://mptran1802.github.io/astrofy',
   integrations: [mdx(), sitemap(), tailwind()]
 });
